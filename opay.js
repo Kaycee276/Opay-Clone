e0695fd3dd7bc7e8109e3ca2e8bc139808fde89c
Kaycee276 <kizuaba@gmail.com>
2023-11-03 22:13:08 +0100

    my first commit

---
+++ opay.js
@@ -0,0 +1,18 @@
+var aa = document.getElementById('subnav2');
+var bb = document.getElementById('subnav3');
+var cc = document.getElementById('lille');
+var dd = document.getElementById('lens');
+function onu() {
+    // aa.style.cssText = 'transform:translateX(1500px);';
+    aa.style.display = 'none';
+};
+function amala() {
+    bb.style.display = 'none';
+};
+var ee = document.getElementById('mode');
+var kyle = document.getElementById('kyle');
+ee.style.display = 'none';
+function chukwu() {
+    ee.style.display = 'block';
+}
+
